734794595007fbc5491390cdd9d4d9fe4911ff43
yujikawa <ykyujikawa@gmail.com>
2025-04-03 11:00:22 +0900

    Modify platforms

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,20 @@
+use tauri::Manager;
+
 mod commands;
 mod models;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_webview_window("main").unwrap();
+            #[cfg(target_os="macos")]
+            window.set_decorations(true)?;
+            #[cfg(target_os="windows")]
+            window.set_decorations(false)?;
+            
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
             commands::get_hardworker,
