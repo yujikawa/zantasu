54c9b9cff98c728e2b9e2d3d1583a93e0959271f
yujikawa <ykyujikawa@gmail.com>
2025-04-07 09:14:18 +0900

    Modify css

--- src/scenes/start.rs
+++ src/scenes/start.rs
@@ -23,7 +23,7 @@ pub fn StartScene(
 
             // === タイトルロゴ ===
             <img src="public/assets/logo/title.png"
-                style="position: absolute; left: 50%; top: -80px; transform: translateX(-50%); width: 400px;" />
+                style="position: absolute; left: 50%; top: 0px; transform: translateX(-50%); width: 400px;" />
 
             <img src="public/assets/characters/rena/start.png"
             style="position: absolute;right: 50px;bottom: 0;height: 500px;" />
